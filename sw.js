5689ac4fb330e40e5df4afa4f166f9045d78d566
yvoxbox799-hue <yvoxbox799@gmail.com>
2026-09-13 10:33:55 +0200

    Mise à jour du cache hors ligne pour la V4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='lautrec-rpg-v3';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon.svg','./assets/hero.svg','./assets/map.svg'];
+const CACHE='lautrec-rpg-v4';
+const ASSETS=['./','./index.html','./styles.css','./v4.css','./app.js','./v4.js','./manifest.webmanifest','./assets/icon.svg','./assets/hero.svg','./assets/map.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
