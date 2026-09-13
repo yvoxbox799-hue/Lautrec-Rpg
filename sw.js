498b3b1d6d3e6f9ce35de3215d6667a554361ec2
yvoxbox799-hue <yvoxbox799@gmail.com>
2026-09-13 10:38:32 +0200

    Actualise le cache pour l'interface V4.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lautrec-rpg-v4';
+const CACHE='lautrec-rpg-v4-1';
 const ASSETS=['./','./index.html','./styles.css','./v4.css','./app.js','./v4.js','./manifest.webmanifest','./assets/icon.svg','./assets/hero.svg','./assets/map.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
